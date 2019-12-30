09dc45391db7223b8ded2b078cebe96510682d82
Christian Barcenas <christian@cbarcenas.com>
2019-12-29 16:27:10 -0800

    Unit tests

--- test/passing/label_option_default_args.ml
+++ test/passing/label_option_default_args.ml
@@ -61,3 +61,11 @@ let (* 0 *) f (* 1 *) ?l: (* 2 *) ((* 3 *) x (* 4 *) : (* 5 *) t (* 6 *) = (* 7 
 let f ?l:(C x = d) = e
 
 let (* 0 *) f (* 1 *) ?l: (* 2 *) ((* 3 *) C (* 4 *) x (* 5 *) = (* 6 *) d (* 7 *)) (* 8 *) = e
+
+let f ?m:((module M : S) = (module U)) = e
+
+let f ?m:((module M) = (module U : S)) = e
+
+let f ?m:((module M : S) = (module struct end)) = e
+
+let f ?m:((module M) = (module struct end : S)) = e
